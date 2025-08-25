9ce029e9ce5f45c10639ff3bbc5bd80cb56dabe6
DalPra0 <153088537+DalPra0@users.noreply.github.com>
2025-08-25 16:22:42 -0300

    Initial commit for ClecProject SwiftUI app
    Add Xcode project files, asset catalogs, and initial SwiftUI app structure with ContentView and ClecProjectApp. This sets up the basic project scaffolding for a new iOS application.

---
+++ ClecProject/ClecProject/ClecProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClecProjectApp.swift
+//  ClecProject
+//
+//  Created by Lucas Dal Pra Brascher on 25/08/25.
+//
+
+import SwiftUI
+
+@main
+struct ClecProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

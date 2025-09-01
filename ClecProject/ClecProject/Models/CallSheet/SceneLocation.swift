99431d993a97e370960f5f8ca369ad2c6b4258c1
Giovanni Strasser <53452941+romm27@users.noreply.github.com>
2025-08-31 21:12:45 -0300

    Fix scene location

---
+++ ClecProject/ClecProject/Models/CallSheet/SceneLocation.swift
@@ -0,0 +1,15 @@
+//
+//  Location.swift
+//  ClecProject
+//
+//  Created by Giovanni Galarda Strasser on 28/08/25.
+//
+
+import Foundation
+
+struct SceneLocation: Codable {
+    var name: String
+    var address: String
+    var latitude: Double
+    var longitude: Double
+}

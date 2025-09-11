230564be1e9ab51cc205e02fe84e5535d90e295a
alsy <alysscln@gmail.com>
2025-09-10 23:06:15 -0300

    onboarding-button
    queria mandar p/ content view

--- ClecProject/ClecProject/Views/Tela Inicial/OnboardingView.swift
+++ ClecProject/ClecProject/Views/Tela Inicial/OnboardingView.swift
@@ -36,13 +36,22 @@ enum OnboardingPage: Int, CaseIterable {
         }
     }
     
+    //botao
+    var hasButton : Bool {
+        switch self {
+        case .thirdPage:
+            return true
+        default:
+            return false
+        }
+    }
 }
 
 struct OnboardingView: View {
     @State private var currentPage = 0
     @State private var isAnimating = false
-    @State private var firstPage = false
-    @State private var secondPage: CGFloat = 0.0
+//    @State private var firstPage = false
+//    @State private var secondPage: CGFloat = 0.0
     @Binding var isUserOldLocal: Bool
     
     var body: some View {
@@ -142,7 +151,7 @@ struct OnboardingView: View {
                 Text(page.title)
                     .font(.system(.title, design: .rounded))
                     .fontWeight(.bold)
-                    .foregroundStyle(.textPrimary)
+                    .foregroundStyle(Color("DesignSystem/OnPrimary"))
                     .multilineTextAlignment(.center)
                     .padding(.horizontal)
                     .opacity(isAnimating ? 1 : 0)
@@ -150,10 +159,10 @@ struct OnboardingView: View {
                     .animation(.spring(dampingFraction: 0.8).delay(0.3), value:
                         isAnimating)
 //                    .rotationEffect(Angle(degrees: 20))
-                
+
                 Text(page.description)
                     .font(.system(.title3, design: .rounded))
-                    .foregroundStyle(.textPrimary)
+                    .foregroundStyle(Color("DesignSystem/OnPrimary"))
                     .fontWeight(.regular)
                     .multilineTextAlignment(.center)
                     .padding(.horizontal, 32)
@@ -162,6 +171,25 @@ struct OnboardingView: View {
                     .animation(.spring(dampingFraction: 0.8).delay(0.3), value:
                         isAnimating)
             
+
+            }
+            
+            
+            if(page.hasButton) {
+                Button {
+                    UserDefaults.standard.set(true, forKey: "isUserOld")
+                    //mandar p/ content view :P
+                } label: {
+                    Text("Começar")
+                        .font(.system(size: 17, weight: .semibold))
+                        .foregroundColor(Color("DesignSystem/OnPrimary"))
+                        .tracking(-0.43)
+                }
+                .padding(.all)
+//                    .frame(height: 56)
+                .background(Color("DesignSystem/Primary"))
+                .cornerRadius(12)
+                .buttonStyle(PlainButtonStyle())
             }
         }
         .padding(.top,50)
